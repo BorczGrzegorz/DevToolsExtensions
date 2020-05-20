4f25c6b23346936b5bacef5599a3f7ef7001f23e
Grzegorz Borcz <grzegorz.borcz@comarch.com>
2020-05-20 23:38:47 +0200

    Handle error in background script

--- AddressesExtension/src/background/app.ts
+++ AddressesExtension/src/background/app.ts
@@ -5,13 +5,17 @@ import { subMenuRender } from './controls/SubMenu';
 import { productListRender } from './components/ProductListRender';
 
 export const runBackground = async (): Promise<void> => {
-  const products: ProductDto[] = await getProducts();
-  console.log(products);
-  const properties: chrome.contextMenus.CreateProperties = {
-    title: 'DevTools',
-    id: 'devtools-context-main',
-  };
+  try {
+    const products: ProductDto[] = await getProducts();
+    console.log(products);
+    const properties: chrome.contextMenus.CreateProperties = {
+      title: 'DevTools',
+      id: 'devtools-context-main',
+    };
 
-  chrome.contextMenus.create(properties);
-  productListRender({ products, parentId: 'devtools-context-main' });
+    chrome.contextMenus.create(properties);
+    productListRender({ products, parentId: 'devtools-context-main' });
+  } catch (error) {
+    console.log(error);
+  }
 };
