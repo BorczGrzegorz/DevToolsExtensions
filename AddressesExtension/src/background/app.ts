1924f6fb9f284bd48e48bc3c0d26fb7507024f1a
Grzegorz <borcz.grzegorz@gmail.com>
2020-05-21 09:20:55 +0200

    Retry getting data

--- AddressesExtension/src/background/app.ts
+++ AddressesExtension/src/background/app.ts
@@ -1,21 +1,34 @@
 /// <reference types="chrome"/>
 
 import { getProducts, ProductDto } from '../api/devToolsApi';
-import { subMenuRender } from './controls/SubMenu';
 import { productListRender } from './components/ProductListRender';
 
 export const runBackground = async (): Promise<void> => {
-  try {
-    const products: ProductDto[] = await getProducts();
-    console.log(products);
-    const properties: chrome.contextMenus.CreateProperties = {
-      title: 'DevTools',
-      id: 'devtools-context-main',
-    };
+  const interval = setInterval(async () => {
+    try {
+      await run();
+      clearInterval(interval);
+    } catch (error) {
+      console.log(error);
+    }
+  }, 60 * 1000);
 
-    chrome.contextMenus.create(properties);
-    productListRender({ products, parentId: 'devtools-context-main' });
+  try {
+    await run();
+    clearInterval(interval);
   } catch (error) {
     console.log(error);
   }
+};
+
+const run = async () => {
+  const products: ProductDto[] = await getProducts();
+  console.log(products);
+  const properties: chrome.contextMenus.CreateProperties = {
+    title: 'DevTools',
+    id: 'devtools-context-main',
+  };
+
+  chrome.contextMenus.create(properties);
+  productListRender({ products, parentId: 'devtools-context-main' });
 };
